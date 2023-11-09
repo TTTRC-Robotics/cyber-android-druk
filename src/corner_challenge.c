7e9f7df24263fc7fecbb52b9565183bb2e983e14
flochre <flopubs@gmail.com>
2023-11-09 17:08:48 +0600

    working code with buffer

--- src/corner_challenge.c
+++ src/corner_challenge.c
@@ -32,12 +32,39 @@ float last_yaw_ = 0.0;
 
 
 int turn_count = 0;
+int flag_driving = 0;
 int flag_turning = 0;
 float start_turning_angle = 0.0;
 int gyro_received_once = 0;
 int uss_received_once = 0;
 
 int movement_phase = 0;
+
+#define BUFFER_SIZE 5
+float circularYaw[BUFFER_SIZE] = {0.0}; // Initialize the buffer with zeros
+int currentIndex = 0;
+
+void updateBuffer(float buffer[], float newValue, int *index) {
+    buffer[*index] = newValue;
+    *index = (*index + 1) % BUFFER_SIZE;
+}
+
+int compare(const void *a, const void *b) {
+    return (*(float*)a > *(float*)b) - (*(float*)a < *(float*)b);
+}
+
+float calculateMedian(float buffer[]) {
+  float sortedBuffer[BUFFER_SIZE];
+  for (int i = 0; i < BUFFER_SIZE; ++i) {
+    sortedBuffer[i] = buffer[i];
+  }
+
+  // Using the qsort function to sort the array
+  qsort(sortedBuffer, BUFFER_SIZE, sizeof(float), compare);
+
+  // Calculating the median
+  return sortedBuffer[BUFFER_SIZE / 2];
+}
 
 void drive_forward(const int wheel_speed){
   set_speed(fd_, motor_left_, -wheel_speed);
@@ -67,25 +94,37 @@ void stop_motors(){
 }
 
 void move_backward_until(const int wheel_speed, const float distance_threshold){
+  if(!flag_driving){
+    flag_driving = 1;
+    printf("Start Driving Backward %f ", front_distance_);
+  }
   if(front_distance_ < distance_threshold){
     drive_backward(wheel_speed);
   } else {
     stop_motors();
     movement_phase += 1;
+    flag_driving = 0;
+    printf("Stop Driving Backward %f\n", front_distance_);
   }
 }
 
 void move_forward_until(const int wheel_speed, const float distance_threshold){
+  if(!flag_driving){
+    flag_driving = 1;
+    printf("Start Driving Forward %f ", front_distance_);
+  }
   if(front_distance_ > distance_threshold){
     drive_forward(wheel_speed);
   } else {
     stop_motors();
     movement_phase += 1;
+    flag_driving = 0;
+    printf("Stop Driving Forward %f\n", front_distance_);
   }
 }
 
 void turn_my_robot(const int wheel_speed, const float angle_degree){
-  if(abs(yaw_ - last_yaw_) > 200){
+  if(abs(yaw_ - last_yaw_) > 350){
     if( yaw_ - last_yaw_ > 0){
       turn_count -= 1; 
     } else {
@@ -93,18 +132,23 @@ void turn_my_robot(const int wheel_speed, const float angle_degree){
     }
   }
 
+  updateBuffer(circularYaw, yaw_, &currentIndex);
+  
+
   float real_yaw = yaw_ + turn_count * 360;
   
-  if(!flag_turning && gyro_received_once){
-    start_turning_angle = real_yaw;
+  if(!flag_turning){
+    start_turning_angle = calculateMedian(circularYaw) + turn_count * 360 ;
     flag_turning = 1;
+
+    printf("Start Turning %f - Yaw : %f", angle_degree, start_turning_angle);
   }
 
   double delta_angle = real_yaw - start_turning_angle;
   double epsilon = delta_angle - angle_degree;
   const double tolerance_degree = 2.5;
 
-  printf (" Delta_angle : %f / aim_angle : %f / Epsilon : %f / real_yaw : %f ", delta_angle, angle_degree, epsilon, real_yaw) ;
+  // printf (" Delta_angle : %f / aim_angle : %f / Epsilon : %f / real_yaw : %f ", delta_angle, angle_degree, epsilon, real_yaw) ;
 
   if(abs(epsilon) > tolerance_degree ){
     // then we want to turn
@@ -117,6 +161,8 @@ void turn_my_robot(const int wheel_speed, const float angle_degree){
     // we want to stop
     stop_motors();
     flag_turning = 0;
+    movement_phase += 1;
+    printf("Stop Turning %f - Yaw : %f \n", angle_degree, real_yaw);
   }
   
   last_yaw_ = yaw_;
@@ -146,7 +192,7 @@ void move_my_robot(){
       break;
     case 5:
       // we want to drive backward until the wall
-      move_backward_until(set_speed_, 160.0);
+      move_backward_until(set_speed_, 145.0);
       break;
     case 6:
       // We want to turn 90 degrees
@@ -168,8 +214,12 @@ void move_my_robot(){
       // we want to drive forward until the middle
       move_forward_until(set_speed_, 90.0);
       break;
-    default:
+    case 11:
       printf("Done\n");
+      movement_phase += 1;
+      break;
+    default:
+      break;
     }
 }
 
@@ -189,7 +239,7 @@ int main ()
   {
     if (millis () > nextTime)
     {
-      printf ("\nOut: %3d / ", count) ;
+      // printf ("\nOut: %3d / ", count) ;
       fflush (stdout) ;
 
       if(0 == count % 2){
@@ -209,13 +259,13 @@ int main ()
     if(is_ultrasonic_new_data(uss_port_)){
       uss_received_once = 1;
       front_distance_ = get_uss_cm(uss_port_);
-      printf(" / uss_cm : %f \n", front_distance_);
+      // printf(" / uss_cm : %f \n", front_distance_);
     }
       
     if(is_gyro_new_data()){
       gyro_received_once = 1;
       yaw_ = get_gyro_yaw();
-      printf(" / Gyro : %f \n", yaw_);
+      // printf(" / Gyro : %f \n", yaw_);
     }
 
   }
