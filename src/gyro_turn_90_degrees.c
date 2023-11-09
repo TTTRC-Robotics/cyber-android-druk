0831f52662197dfc95a67e9194fef4227bc05b89
flochre <flopubs@gmail.com>
2023-11-09 15:42:53 +0600

    code for turning 90 degrees

---
+++ src/gyro_turn_90_degrees.c
@@ -0,0 +1,153 @@
+/*
+ * cyberAndroid.c:
+ *	Very simple program to run a robot on the serial port. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+#include <wiringPi.h>
+#include <wiringSerial.h>
+
+#include <serial_megapi/serial_megapi.h>
+
+int turn_count = 0;
+int flag_turning = 0;
+float start_turning_angle = 0.0;
+float last_yaw = 0.0;
+int gyro_received_once = 0;
+
+void drive_forward(const int fd, const int motor_left, const int motor_right, const int wheel_speed){
+  set_speed(fd, motor_left, -wheel_speed);
+  set_speed(fd, motor_right, wheel_speed);
+}
+
+void turn_left(const int fd, const int motor_left, const int motor_right, const int wheel_speed){
+  set_speed(fd, motor_left, wheel_speed);
+  set_speed(fd, motor_right, wheel_speed);
+}
+
+void turn_right(const int fd, const int motor_left, const int motor_right, const int wheel_speed){
+  set_speed(fd, motor_left, -wheel_speed);
+  set_speed(fd, motor_right, -wheel_speed);
+}
+
+void stop_motors(const int fd){
+  set_speed(fd, 1, 0);
+  set_speed(fd, 2, 0);
+  set_speed(fd, 3, 0);
+  set_speed(fd, 4, 0);
+}
+
+void move_my_robot(const int fd, float distance, float yaw, const int motor_left, const int motor_right, const int wheel_speed){
+  if(distance > 20){
+    drive_forward(fd, motor_left, motor_right, wheel_speed);
+  } else {
+    stop_motors(fd);
+  }
+}
+
+void turn_my_robot(const int fd, float yaw, const int motor_left, const int motor_right, const int wheel_speed, const float angle_degree){
+  if(abs(yaw - last_yaw) > 200){
+    if( yaw - last_yaw > 0){
+      turn_count -= 1; 
+    } else {
+      turn_count += 1;
+    }
+  }
+
+  float real_yaw = yaw + turn_count * 360;
+  
+  if(!flag_turning && gyro_received_once){
+    start_turning_angle = real_yaw;
+    flag_turning = 1;
+  }
+
+  double delta_angle = real_yaw - start_turning_angle;
+  double epsilon = delta_angle - angle_degree;
+  const double tolerance_degree = 2.5;
+
+  printf (" Delta_angle : %f / aim_angle : %f / Epsilon : %f / real_yaw : %f ", delta_angle, angle_degree, epsilon, real_yaw) ;
+
+  if(abs(epsilon) > tolerance_degree ){
+    // then we want to turn
+    if(epsilon < 0){
+      turn_left(fd, motor_left, motor_right, wheel_speed);
+    } else {
+      turn_right(fd, motor_left, motor_right, wheel_speed);
+    }
+  } else {
+    // we want to stop
+    stop_motors(fd);
+    // flag_turning = 0;
+  }
+  
+  last_yaw = yaw;
+}
+
+int main ()
+{
+  int fd;
+  int count=0;
+  unsigned int nextTime ;
+
+  if ((fd = init_serial("/dev/ttyAMA0", 115200)) < 0){
+    fprintf (stderr, "Unable to open serial device: %s\n", strerror (errno)) ;
+    return 1 ;
+  }
+
+  const int uss_port = 0x7;
+
+  const int motor_left_port = 3;
+  const int motor_right_port = 2;
+  const int max_speed = 255;
+  const int min_speed = 15;
+  const int set_speed = 30;
+
+  const float desire_turn_angle = -90.0;
+
+  float front_ditance = 0.0;
+  float gyro_yaw = 0.0;
+
+  nextTime = millis () + 300 ;
+
+  for (;;)
+  {
+    if (millis () > nextTime)
+    {
+      printf ("\nOut: %3d / %d / ", count, fd) ;
+      fflush (stdout) ;
+
+      if(0 == count % 2){
+        request_gyro(fd, GYRO_ALL_AXES);
+      } else {
+        request_uss(fd, uss_port);
+      }
+      
+      // move_my_robot(fd, front_ditance, gyro_yaw, motor_left_port, motor_right_port, set_speed);
+      turn_my_robot(fd, gyro_yaw, motor_left_port, motor_right_port, set_speed, desire_turn_angle);
+      
+      nextTime += 100 ;
+      ++count ;
+    }
+
+    if(is_ultrasonic_new_data(uss_port)){
+      front_ditance = get_uss_cm(uss_port);
+      printf(" / uss_cm : %f \n", front_ditance);
+    }
+      
+    if(is_gyro_new_data()){
+      gyro_received_once = 1;
+      gyro_yaw = get_gyro_yaw();
+      printf(" / Gyro : %f \n", gyro_yaw);
+    }
+
+  }
+
+  printf ("\n") ;
+  return 0 ;
+}
